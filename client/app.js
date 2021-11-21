80a6310bf553dfbed0f966b60775baed1ea24567
aaWang27 <aatwang27@gmail.com>
2021-11-21 11:32:45 -0700

    getBath and getBHK for user input

--- client/app.js
+++ client/app.js
@@ -1,3 +1,35 @@
+function getBathValue() {
+    var uiBath = document.getElementsByName("uiBath");
+    for(var i in uiBath) {
+        if(uiBath[i].checked) {
+            return parseInt(i)+1;
+        }
+    }
+    return -1;
+}
+
+function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for(var i in uiBHK) {
+        if(uiBHK[i].checked) {
+            return parseInt(i)+1;
+        }
+    }
+    return -1;
+}
+
+function onClickedEstimatePrice() {
+    console.log("Estimate Price Button Clicked");
+    location = document.getElementsByName("uiLocations");
+    sqft = document.getElementsByName("uiSquareft");
+    bath = getBathValue();
+    bhk = getBHKValue();
+    var url = "http://127.0.0.1:5000/get_estimated_price";
+    $.get(url, function(data, status) {
+        
+    });
+}
+
 function onPageLoad() {
     console.log("loading document")
     var url = "http://127.0.0.1:5000/get_location_names";
@@ -17,6 +49,3 @@ function onPageLoad() {
 
 window.onload = onPageLoad;
 
-function onClickedEstimatePrice() {
-    var url = "http://127.0.0.1:5000/get_estimated_price";
-}
