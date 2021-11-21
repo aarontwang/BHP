859dd9bdf8b65d30ff2e77dbc70589076e28fe19
aaWang27 <aatwang27@gmail.com>
2021-11-20 23:07:14 -0700

    added location options

---
+++ client/app.js
@@ -0,0 +1,22 @@
+function onPageLoad() {
+    console.log("loading document")
+    var url = "http://127.0.0.1:5000/get_location_names";
+    $.get(url, function(data, status) {
+        console.log("got response for get_location_names request");
+        if(data) {
+            var locations = data.locations;
+            var uiLocations = document.getElementById("uiLocations");
+            $('#uiLocations').empty();
+            for(var i in locations) {
+                var opt = new Option(locations[i]);
+                $('#uiLocations').append(opt);
+            }
+        }
+    });
+}
+
+window.onload = onPageLoad;
+
+function onClickedEstimatePrice() {
+    var url = "http://127.0.0.1:5000/get_estimated_price";
+}
